7e270474bb6c959ca566ff60c37b6f911f18c2aa
sakshay42 <sakshaay42@gmail.com>
2025-02-12 16:25:53 -0500

    Create 209.py

---
+++ 209.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        out = float('inf') #infinite
+        window_sum = 0
+        left = 0
+        for right in range(0,len(nums)):
+            window_sum += nums[right]
+            while total >= target:
+                out = min(out,right-left+1) 
+                window_sum -= nums[left]
+                left +=  1
+        return(out if out!= float('inf') else 0) #short hand to write if else
