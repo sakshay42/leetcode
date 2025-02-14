24c6a9bd2215f1ae29794480d33a531efe84a100
sakshay42 <sakshaay42@gmail.com>
2025-02-14 12:52:48 -0500

    Update 26.py

--- 26.py
+++ 26.py
@@ -10,3 +10,17 @@ class Solution:
 
             bottom +=1
          return(top+1)
+
+
+# below faster - because fewer swaps and no pointers
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        k = 0
+
+        for x in nums:
+            if k == 0 or x != nums[k - 1]:
+               nums[k] = x
+               k += 1
+
+        return k
