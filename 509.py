5c1db6c521de16b751e03c89d052aa3ad01f12c6
sakshay42 <sakshaay42@gmail.com>
2025-02-17 18:24:52 -0500

    Create 509.py

---
+++ 509.py
@@ -0,0 +1,8 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n==0 or n==1: return(n)
+        memo = {0:0,1:1}
+        for i in range(2,n+1):
+            memo[i] = memo[i-1] + memo[i-2]
+        
+        return(memo[n])
