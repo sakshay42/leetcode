56649af4e7fdce5970e46c1a98967055da027259
sakshay42 <sakshaay42@gmail.com>
2025-02-17 18:28:23 -0500

    Update 509.py

--- 509.py
+++ 509.py
@@ -1,4 +1,6 @@
 class Solution:
+    
+    """
     def fib(self, n: int) -> int:
         if n==0 or n==1: return(n)
         memo = {0:0,1:1}
@@ -6,3 +8,14 @@ class Solution:
             memo[i] = memo[i-1] + memo[i-2]
         
         return(memo[n])
+    """
+
+#without using dictionary
+    def fib(self, n: int) -> int:
+        prev = 0
+        current = 1
+        if n==0 or n==1: return(n)
+        for i in range(n-1):
+            prev,current = current,current + prev
+        return(current)
+            
