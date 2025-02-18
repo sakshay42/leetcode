2062001a1c33e75f4491e687e2a7c3d7411c1320
sakshay42 <sakshaay42@gmail.com>
2025-02-17 19:08:47 -0500

    Update 746.py

--- 746.py
+++ 746.py
@@ -1,4 +1,5 @@
 class Solution:
+    """
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         prev =0
         current = 0
@@ -6,3 +7,10 @@ class Solution:
         for i in range(2,n+1):
             prev,current = current, min(prev+cost[i-2],current+cost[i-1])
         return(current)
+        """
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        stair_cost = [0]*(n+1)
+        for i in range(2,n+1):
+            stair_cost[i] = min(stair_cost[i-1]+cost[i-1],stair_cost[i-2]+cost[i-2])
+        return stair_cost[n]
