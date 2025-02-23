a57b129443bf399a5a129067a277788bb976baa8
sakshay42 <sakshaay42@gmail.com>
2025-02-23 17:20:44 -0500

    Update 413.py

--- 413.py
+++ 413.py
@@ -8,3 +8,18 @@ class Solution:
                 out += current # if there are k consecutive windows of size k then 1+2+..+k ap subarrays of size >=3 
             else: current = 0 #not an ap then move the window
         return(out)
+
+
+
+#same but left and right pointers
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        left =0 
+        out = 0
+        for right in range(1,len(nums)-1):
+            if nums[right-1] + nums[right+1] == 2*nums[right]:
+                out += right-left
+            else: 
+                left = right
+        
+        return out
