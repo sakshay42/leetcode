9878262284bbbfc1029cb2a7f26fcd0dfb3cf3ad
sakshay42 <sakshaay42@gmail.com>
2025-02-23 18:42:39 -0500

    Update 128.py

--- 128.py
+++ 128.py
@@ -12,3 +12,32 @@ class Solution:
                 dictionary[i + right] = current
                 out = max(out,current)
         return out
+
+
+######## quick
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        # edge cases
+        # nums is empty, return 0
+        if not nums:
+            return 0
+
+        # duplicate not included
+        unique_num = set(nums)
+
+        max_len = 1
+        curr_len = 0
+
+        for num in unique_num:
+            if (num - 1) not in unique_num: # num -1 not in the list, then num is the start of the subsequence
+                curr_num = num
+                curr_len = 1 # new sequence
+                # expand this subsequence, check curr_num + 1
+                while (curr_num + 1) in unique_num:
+                    curr_len += 1
+                    curr_num += 1 # move to next num
+
+                max_len = max(max_len, curr_len)
+
+        return max_len 
