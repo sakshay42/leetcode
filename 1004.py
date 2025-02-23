c2a02fa418e6477323a934ad4a1628cbe9250e68
sakshay42 <sakshaay42@gmail.com>
2025-02-23 18:22:43 -0500

    Update 1004.py

--- 1004.py
+++ 1004.py
@@ -26,6 +26,7 @@ class Solution:
         return out
 """
 
+""""
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
         out = left = 0
@@ -38,3 +39,17 @@ class Solution:
                  left += 1  
             out = max(out,right - left +1)
         return out
+""""
+
+
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        l=r=0    
+        for r in range(len(nums)):
+            if nums[r] == 0:
+                k-=1
+            if k<0:
+                if nums[l] == 0:
+                    k+=1
+                l+=1
+        return r-l+1
