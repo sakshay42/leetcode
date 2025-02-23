c913e9b011935228c94589c46aed3862e96b2b05
sakshay42 <sakshaay42@gmail.com>
2025-02-23 16:39:39 -0500

    Update 3.py

--- 3.py
+++ 3.py
@@ -11,3 +11,19 @@ class Solution:
             window += s[right]
             out = max(out,right- left +1)
         return(out)
+
+
+#same using sets
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        window = set()
+        left =0
+        out = 0
+        for right in range(len(s)):
+            while s[right] in window:
+                    window.remove(s[left])
+                    left +=1
+            window.add(s[right])
+            out = max(right-left+1,out)
+        return out
