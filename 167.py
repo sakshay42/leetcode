e6db0a150743f7333a5fc02e8b6eb2f9e2600e45
sakshay42 <sakshaay42@gmail.com>
2025-02-23 16:43:46 -0500

    Update 167.py

--- 167.py
+++ 167.py
@@ -9,4 +9,18 @@ class Solution:
             if  x+y  == target: return [left+1,right+1]
             elif x + y > target: right = right -1
             else: left = left + 1
+
+## Just store sum 
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        n = len(numbers)
+        left= 0
+        right = n-1
+        while right > left:
+            total = numbers[left] + numbers[right]
+            if  total == target:
+                return [left+1,right+1]
+            elif total > target: right -=1
+            else: left +=1
+        
         
